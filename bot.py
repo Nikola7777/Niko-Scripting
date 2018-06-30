eb2b9b678b087c4159aedfda57ba4b5cf4cbee3d
Nikola7777 <40716840+Nikola7777@users.noreply.github.com>
2018-06-30 16:56:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -91,4 +91,4 @@ async def Mamamia(ctx):
     for i in range (0,10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000):
         await bot.say("Niko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is best\nNiko is bestaw")
 
-bot.run("NDUzOTY1MTQ4MTAxNTQxOTAw.Df33LA.Sg6IhLBwRNXeWt5RBTF_3QoD-G4")
+bot(str(os.environ.get('TOKEN')))
